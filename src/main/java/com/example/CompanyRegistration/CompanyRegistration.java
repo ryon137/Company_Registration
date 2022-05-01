c9786eaea7c479f6109bc7953ffc661690d25612
yby1997 <frmpjmattlemage@gmail.com>
2022-04-30 22:16:26 -0700

    added eureka client

--- src/main/java/com/example/CompanyRegistration/CompanyRegistration.java
+++ src/main/java/com/example/CompanyRegistration/CompanyRegistration.java
@@ -2,8 +2,10 @@ package com.example.CompanyRegistration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CompanyRegistration {
 
 	public static void main(String[] args) {
